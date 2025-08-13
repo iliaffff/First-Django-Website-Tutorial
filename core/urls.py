17418e6cbefae361136cc7673cf83c9a71ab00fa
Ilia Fallah <iliafallah138413@gmail.com>
2025-08-13 17:44:03 +0330

    A few changes for the view

--- core/urls.py
+++ core/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from .views import homePage
+from .views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", homePage)
+    path("", homePage),
+    path("json-test", jsonTest),
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 def homePage(request):
     return HttpResponse("<h1>This is home page</h1>")
+
+def jsonTest(request):
+    return JsonResponse({"name": "Ilia", "age": 19})
+    
